bc92340aa1bd5680d92e27a5c2c2d73f9ae0b10f
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2011-10-29 01:01:21 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-easy-gallery@456918 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/admin/overview.php
+++ trunk/admin/overview.php
@@ -47,5 +47,10 @@ $galleryResults = $wpdb->get_results( "SELECT * FROM wp_easy_gallery" );
 			<?php } ?>
         </tbody>
      </table>
-     <p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PMZ2FPNJPH59U" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></a></p>
+     <br />
+<p><strong>Try Custom Post Donations Pro</strong><br /><em>This WordPress plugin will allow you to create unique customized PayPal donation widgets to insert into your WordPress posts or pages and accept donations.</em></p>
+<p><a href="http://labs.hahncreativegroup.com/wordpress-plugins/custom-post-donations-pro/"><img src="http://labs.hahncreativegroup.com/wp-content/uploads/2011/10/CustomPostDonationsPro-Banner.gif" width="374" height="60" border="0" alt="Custom Post Donations Pro" /></a></p>
+<br />
+<p><em>Please consider donating to the continued development of this plugin. Thanks.</em></p>
+<p><a href="https://www.paypal.com/cgi-bin/webscr?cmd=_s-xclick&hosted_button_id=PMZ2FPNJPH59U" target="_blank"><img src="https://www.paypalobjects.com/en_US/i/btn/btn_donateCC_LG.gif" border="0" alt="PayPal - The safer, easier way to pay online!"><img alt="" border="0" src="https://www.paypalobjects.com/en_US/i/scr/pixel.gif" width="1" height="1"></a></p>
 </div>
