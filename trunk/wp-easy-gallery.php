20fa893cfa30117a50fa64921898426e5369f863
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2012-02-26 05:24:54 +0000

    git-svn-id: https://plugins.svn.wordpress.org/wp-easy-gallery@510621 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- trunk/wp-easy-gallery.php
+++ trunk/wp-easy-gallery.php
@@ -4,7 +4,7 @@
 	Plugin URI: http://labs.hahncreativegroup.com/wordpress-plugins/easy-gallery/
 	Description: Wordpress Plugin for creating dynamic photo galleries	
 	Author: HahnCreativeGroup
-	Version: 1.3.1
+	Version: 1.4
 	Author URI: http://labs.hahncreativegroup.com/
 	*/
 	
