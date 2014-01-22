88a4a9e73f4fae17c74586b3ddf6df88b4d726b2
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-01-22 06:11:07 +0000

    Removed HcgGalleryLoader.js and updated EasyGalleryLoader.js files
    git-svn-id: https://plugins.svn.wordpress.org/wp-easy-gallery/trunk@842862 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- js/EasyGalleryLoader_hideOverlay.js
+++ js/EasyGalleryLoader_hideOverlay.js
@@ -1,5 +1,7 @@
-// WP Easy Gallery 3.5
+/*
+// WP Easy Gallery
 // http://labs.hahncreativegroup.com/wordpress-plugins/easy-gallery/
+*/ http://labs.hahncreativegroup.com/wordpress-plugins/easy-gallery/
 
 jQuery(document).ready(function(){			
 	jQuery(".gallery a[rel^='prettyPhoto']").prettyPhoto({counter_separator_label:' of ',theme:'light_rounded',overlay_gallery:false});
--- js/HcgGalleryLoader.js
+++
@@ -1,3 +0,0 @@
-jQuery(document).ready(function(){			
-			jQuery(".gallery a[rel^='prettyPhoto']").prettyPhoto({counter_separator_label:' of ',theme:'light_rounded',overlay_gallery:true});
-		});
