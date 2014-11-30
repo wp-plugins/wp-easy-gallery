1fa1338fc202e600ea1e9b3bb1d8d5b3653f81ed
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2014-11-30 00:21:19 +0000

    version 4.0.1 - fixing RSS formatting issue
    git-svn-id: https://plugins.svn.wordpress.org/wp-easy-gallery/trunk@1035201 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- admin/rss.php
+++ admin/rss.php
@@ -1,23 +1,32 @@
 <?php
 $url = "http://labs.hahncreativegroup.com/feed/";
-$rss = simplexml_load_file($url);
-if($rss)
-{
-  echo '<h3>'.$rss->channel->title.'</h3>';
-  $items = $rss->channel->item;
-  $count = 0;
-  foreach($items as $item)
-  {
-	$count++;	
-	$title = $item->title;
-	$link = $item->link;
-	$published_on = $item->pubDate;
-	$description = $item->description;
-	echo '<h4><a href="'.$link.'">'.$title.'</a></h4>';
-	echo '<p>'.$description.'</p>';
-	if ($count >= 5) {
-		break;	
+$default = '<h3>Visit <a href="http://labs.hahncreativegroup.com" target="_blank">http://labs.hahncreativegroup.com</a> for news and info</h3>';
+
+try {
+	$rss = simplexml_load_file($url);
+	if($rss)
+	{
+		echo '<h3>'.$rss->channel->title.'</h3>';
+		$items = $rss->channel->item;
+		$count = 0;
+		foreach($items as $item)
+		{
+			$count++;	
+			$title = $item->title;
+			$link = $item->link;
+			$published_on = $item->pubDate;
+			$description = $item->description;
+			echo '<h4><a href="'.$link.'">'.$title.'</a></h4>';
+			echo '<p>'.$description.'</p>';
+			if ($count >= 3) {
+				break;	
+			}
+		}
+	} else {
+		echo $default;
 	}
-  }
+}
+catch(Exception $e) {
+	echo $default;
 }
 ?>
