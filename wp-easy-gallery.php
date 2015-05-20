2d5e1492f6e8adfd333e9953474844c9536aea80
hahncgdev <hahncgdev@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-05-20 14:41:25 +0000

    updated prettyPhoto.js to current version to fix XSS vulnerability
    git-svn-id: https://plugins.svn.wordpress.org/wp-easy-gallery/trunk@1164193 b8457f37-d9ea-0310-8a92-e5e31aec5664

--- wp-easy-gallery.php
+++ wp-easy-gallery.php
@@ -4,7 +4,7 @@
 	Plugin URI: http://labs.hahncreativegroup.com/wordpress-gallery-plugin/
 	Description: Wordpress Plugin for creating dynamic photo galleries	
 	Author: HahnCreativeGroup
-	Version: 4.1
+	Version: 4.1.1
 	Author URI: http://labs.hahncreativegroup.com/wordpress-plugins/easy-gallery/
 	*/	
 	
